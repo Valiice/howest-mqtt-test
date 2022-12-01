f5899f1b3491ac508dffd5b9e9f14d2b68b474c9
Valiice <valentinclaes@live.be>
2022-12-01 17:41:14 +0100

    Init

---
+++ MQTT-Publisher.Cons/Publisher.cs
@@ -0,0 +1,47 @@
+﻿using MQTTnet;
+using MQTTnet.Client;
+using System;
+
+namespace MQTT_Test.Cons
+{
+    internal class Publisher
+    {
+        static async Task Main(string[] args){
+            
+            var mqttFactory = new MqttFactory();
+            IMqttClient client = mqttFactory.CreateMqttClient();
+            var options = new MqttClientOptionsBuilder()
+                            .WithClientId(Guid.NewGuid().ToString())
+                            .WithTcpServer("test.mosquitto.org", 1883)
+                            .WithCleanSession()
+                            .Build();
+            client.ConnectedAsync += async e => MqttOnConnected(e);
+            client.DisconnectedAsync += async e => MqttOnDisconnected(e);
+
+            await client.ConnectAsync(options);
+
+            Console.WriteLine("Please press a key to publish the message");
+
+            string payload = Console.ReadLine();
+
+            await PublicMessageAsync(client, payload);
+
+            await client.DisconnectAsync();
+        }
+
+        private static async Task PublicMessageAsync(IMqttClient client, string payload)
+        {
+            string messagePayload = payload;
+            var message = new MqttApplicationMessageBuilder()
+                .WithTopic("Howest/A")
+                .WithPayload(messagePayload)
+                .WithQualityOfServiceLevel(MQTTnet.Protocol.MqttQualityOfServiceLevel.AtLeastOnce)
+                .Build();
+
+            if (client.IsConnected)
+                await client.PublishAsync(message);
+        }
+        private static void MqttOnConnected(MqttClientConnectedEventArgs e) => Console.WriteLine($"MQTT Client: Connected with result: {e.ConnectResult.ResultCode}");
+        private static void MqttOnDisconnected(MqttClientDisconnectedEventArgs e) => Console.WriteLine($"MQTT Client: Broker connection lost with reason: {e.Reason}.");
+    }
+}
---
+++ MQTT-Subscriber.Cons/Subscriber.cs
@@ -0,0 +1,43 @@
+﻿using MQTTnet;
+using MQTTnet.Client;
+using System.Diagnostics;
+using System.Text;
+
+namespace MQTT_Subscriber.Cons
+{
+    internal class Subscriber
+    {
+        static async Task Main(string[] args)
+        {
+            var mqttFactory = new MqttFactory();
+            IMqttClient client = mqttFactory.CreateMqttClient();
+            var options = new MqttClientOptionsBuilder()
+                            .WithClientId(Guid.NewGuid().ToString())
+                            .WithTcpServer("test.mosquitto.org", 1883)
+                            .WithCleanSession()
+                            .Build();
+            client.ConnectedAsync += async e => await MqttOnConnected(e, client);
+            client.DisconnectedAsync += async e => MqttOnDisconnected(e);
+            client.ApplicationMessageReceivedAsync += async e => MqttOnMessageReceived(e);
+
+            await client.ConnectAsync(options);
+
+            Console.ReadLine();
+
+            await client.DisconnectAsync();
+        }
+        private static async Task MqttOnConnected(MqttClientConnectedEventArgs e, IMqttClient client)
+        {
+            Console.WriteLine($"MQTT Client: Connected with result: {e.ConnectResult.ResultCode}");
+            var topicFilter = new MqttTopicFilterBuilder()
+                                                        .WithTopic("Howest/A")
+                                                        .Build();
+            await client.SubscribeAsync(topicFilter);
+        }
+        private static void MqttOnDisconnected(MqttClientDisconnectedEventArgs e)
+        {
+            Console.WriteLine($"MQTT Client: Broker connection lost with reason: {e.Reason}.");
+        }
+        private static void MqttOnMessageReceived(MqttApplicationMessageReceivedEventArgs e) => Console.WriteLine($"MQTT Client: Received Message: {Encoding.UTF8.GetString(e.ApplicationMessage.Payload)}.");
+    }
+}
